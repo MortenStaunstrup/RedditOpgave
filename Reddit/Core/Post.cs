d5d8596444417d063c01366dad7a17afecdc59f5
MortenStaunstrup <mort490fgromling@gmail.com>
2025-10-09 17:47:03 +0200

    Added URL option for user

--- Reddit/Core/Post.cs
+++ Reddit/Core/Post.cs
@@ -9,8 +9,9 @@ public class Post
     [MaxLength(400)]
     public string Title { get; set; }
     public string Author { get; set; }
-    [MaxLength(800)]
+    [MaxLength(800, ErrorMessage = "Maximum length of content is 800")]
     public string? Content { get; set; }
+    [MaxLength(500, ErrorMessage = "Maximum length of ULR is 500")]
     public string? URL { get; set; }
     public DateTime TimeStamp { get; set; }
     public List<Comment> Comments { get; set; } = new List<Comment>();
